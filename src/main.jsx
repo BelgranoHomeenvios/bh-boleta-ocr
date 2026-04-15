237f89150b70130e25a95eb81fb93f6b3f2a5520
BelgranoHomeenvios <brian@belgranohome.com>
2026-04-15 17:05:00 +0000

    Setup Vite + React project for Vercel deploy

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BHBoletaOCR from './bh_boleta_ocr'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BHBoletaOCR />
+  </React.StrictMode>,
+)
